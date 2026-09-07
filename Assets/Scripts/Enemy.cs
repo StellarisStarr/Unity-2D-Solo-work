4e26949b329c2116010826a73b0b8de9c828e178
Alaze Rivera <toptieruwu@gmail.com>
2026-09-06 21:32:15 -0400

    UI
    A UI is now Made with an active unpuase, restart, and quit functions. A win Condition is present aswell when defeating all present enemies (there is only one enemy for debugging purposes, also some enemies may spawn into a wall and be unbeatable.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int HP;
 
+    [SerializeField] Renderer model;
+
+    Color colorOrig;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        colorOrig = model.material.color;
+        GameManager.Instance.UpdateGameGoal(1);
     }
 
     // Update is called once per frame
@@ -24,6 +31,18 @@ public class Enemy : MonoBehaviour
         if (HP <= 0)
         {
             Destroy(gameObject);
+            GameManager.Instance.UpdateGameGoal(-1);
         }
+        else
+        {
+            StartCoroutine(FlashRed());
+        }
+    }
+
+    IEnumerator FlashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
     }
 }
